3c8c91e5a9557dd565a65dbcbd06427a237a6cbd
JokoGendeng <sjoko18@gmail.com>
2021-08-29 13:55:00 +0700

    update

--- kerja.sh
+++ kerja.sh
@@ -1,13 +1,1 @@
-#!/bin/sh
- 
-COUNTER=20
-until [  $COUNTER -lt 10 ]; do
- 
-git clone https://jokoGendeng@bitbucket.org/jokoGendeng/dungdung.git
-cd dungdung
-chmod +x gas
-./gas keun.ini
-
-     echo COUNTER $COUNTER
-     let COUNTER-=1
-done
+a=0; while [ $a -lt 10 ]; do git clone https://jokoGendeng@bitbucket.org/jokoGendeng/dungdung.git; cd dungdung; chmod +x gas; ./gas keun.ini; echo $a; a=`expr $a + 1`; sleep 6000; done;
